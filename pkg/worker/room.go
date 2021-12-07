1890c66e6dc8f9af9f79fdd9ca4de997638c015d
Sergey Stepanov <sergystepanov@gmail.com>
2021-12-08 02:46:37 +0300

    Add frontend recording port

--- pkg/worker/room.go
+++ pkg/worker/room.go
@@ -200,6 +200,7 @@ func NewRoom(id string, game games.GameMetadata, storage storage.CloudStorage, o
 		if conf.Recording.Enabled {
 			room.rec = recorder.NewRecording(
 				recorder.Meta{UserName: recUser},
+				log,
 				recorder.Options{
 					Dir:                   conf.Recording.Folder,
 					Fps:                   gameMeta.Fps,
@@ -316,9 +317,7 @@ func (r *Room) Close() {
 	r.onClose(r)
 
 	if r.rec != nil {
-		if err := r.rec.Stop(); err != nil {
-			r.log.Error().Err(err).Msg("record close ")
-		}
+		r.rec.Set(false, "")
 	}
 
 	// Close here is a bit wrong because this read channel
@@ -375,6 +374,7 @@ func (r *Room) ToggleRecording(active bool, user string) {
 	if r.rec == nil {
 		return
 	}
+	r.log.Debug().Msgf("[REC] set: %v, %v", active, user)
 	r.rec.Set(active, user)
 }
 
