giongto35/cloud-game

Close WebRTC connections on disconnect
Not closing RTCPeerConnection and co will cause eventually high processor load in Chrome.
Also, Chrome has some GC issues related to WebRTC, see: https://bugs.chromium.org/p/chromium/issues/detail?id=825576.

web/js/controller.js
@@ -449,6 +449,7 @@
     event.sub(CONNECTION_CLOSED, () => {
         input.poll().disable();
         socket.abort();
+        rtcp.stop();
     });
     event.sub(LATENCY_CHECK_REQUESTED, onLatencyCheck);
     event.sub(GAMEPAD_CONNECTED, () => message.show('Gamepad connected'));
@@ -472,4 +473,4 @@
 
     // initial app state
     setState(app.state.eden);
-})(document, event, env, gameList, input, KEY, log, message, recording, room, settings, socket, stats, stream, utils);
+})(document, event, env, gameList, input, KEY, log, message, recording, room, rtcp, settings, socket, stats, stream, utils);

windmeup/giongto35-cloud-game

web/js/controller.js
@@ -512,6 +512,7 @@
             clearInterval(pingPong);
             pingPong = 0;
         }
+        webrtc.stop();
     });
     event.sub(LATENCY_CHECK_REQUESTED, onLatencyCheck);
     event.sub(GAMEPAD_CONNECTED, () => message.show('Gamepad connected'));
@@ -542,4 +543,4 @@
 
     // initial app state
     setState(app.state.eden);
-})(document, event, env, gameList, input, KEY, log, message, recording, room, settings, socket, stats, stream, utils);
+})(document, event, env, gameList, input, KEY, log, message, recording, room, settings, socket, stats, stream, utils, webrtc);